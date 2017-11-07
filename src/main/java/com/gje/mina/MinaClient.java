0a591b6d3a513ae6b18502cc23e597751f487116
笑红尘 <gjw.5@qq.com>
2017-11-07 23:15:47 +0800

    测试

--- src/main/java/com/gje/mina/MinaClient.java
+++ src/main/java/com/gje/mina/MinaClient.java
@@ -16,13 +16,9 @@ public class MinaClient {
 	private static final String HOSTNAME = "192.168.1.102";
 	private static final int PORT = 1234;
 	private static final long CONNECT_TIMEOUT = 30*1000L; // 30 seconds
-	
-	public static void main(String[] args) {
-		sendData();
-	}
-	
-	public static void sendData() {
-		NioSocketConnector connector = new NioSocketConnector();
+	static NioSocketConnector connector = new NioSocketConnector();
+	static
+	{
 		//将连接超时值设置为毫秒
 		connector.setConnectTimeoutMillis(CONNECT_TIMEOUT);
 		//指定过滤器
@@ -31,6 +27,20 @@ public class MinaClient {
 				new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
 		//指定处理器
 		connector.setHandler(new TimeClientHandler());
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		for (int i = 0; i < 60000; i++) {
+			sendData();
+		}
+		//彻底释放Session,退出程序时调用不需要重连的可以调用这句话，也就是短连接不需要重连。长连接不要调用这句话，注释掉就OK。
+		Thread.sleep(5000);
+		connector.dispose();//释放所有资源
+	}
+	
+	public static void sendData() {
+		
+		
 		
 		IoSession session = null;
 		//死循环直到链接成功
@@ -38,6 +48,7 @@ public class MinaClient {
 			ConnectFuture future = connector.connect(new InetSocketAddress(HOSTNAME, PORT));
 			future.awaitUninterruptibly();//等待异步操作完成
 			session = future.getSession();
+			System.out.println(">>>>客户端链接对象："+session);
 		} catch (RuntimeIoException e) {
 			e.printStackTrace();
 		}
@@ -53,6 +64,5 @@ public class MinaClient {
 		{
 			System.out.println("关闭连接");
 		}
-		connector.dispose();//释放所有资源
 	}
 }
